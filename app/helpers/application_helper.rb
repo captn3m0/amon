2a07c525252beb074bb89814a32aef1745b23b12
Abhay Rana <capt.n3m0@gmail.com>
2014-08-18 23:58:10 +0530

    Force utf8 encoding on messages for now.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ require 'email_reply_parser'
 module ApplicationHelper
 
   def base64_decode(text)
-    text.tr('-_','+/').unpack('m')[0]
+    text.tr('-_','+/').unpack('m')[0].force_encoding('utf-8')
   end
 
   def parse_email_body(body)
